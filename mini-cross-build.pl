52346b8540939046c2e2f87e8ad96d5cae62f9d6
Holden Karau <holden@pigscanfly.ca>
2021-09-21 17:25:23 -0700

    Wait we don't use any internals, with public APIs we should be binary compatible within a release line.

--- mini-cross-build.pl
+++ mini-cross-build.pl
@@ -3,9 +3,9 @@ use File::Slurp;
 use strict;
 use warnings;
 my @spark_versions = (
-    "2.4.0", "2.4.1", "2.4.2", "2.4.3", "2.4.4", "2.4.5", "2.4.6", "2.4.7",
-    "3.0.0", "3.0.1", "3.0.2",
-    "3.1.1", "3.1.2"
+    "2.4.0",
+    "3.0.0",
+    "3.1.1", # Special 3.1.0 was not properly released.
     );
 # Backup the build file
 `cp build.sbt build.sbt_back`;
@@ -15,9 +15,9 @@ foreach my $spark_version (@spark_versions) {
     print "Next spark version ".$spark_version;
     print "\nbuilding\n";
     # Publish local first so kafka sub project can resolve
-    print "\nGoing to run: ./build/sbt  -DsparkVersion=$spark_version version clean +publishLocal\n";
-    print `./build/sbt  -DsparkVersion=$spark_version version clean +publishLocal`;
-    print "\nGoing to run: ./build/sbt  -DsparkVersion=$spark_version version clean +publishSigned\n";
-    print `./build/sbt  -DsparkVersion=$spark_version version clean +publishSigned`;
+    print "\nGoing to run: sbt  -DsparkVersion=$spark_version version clean +publishLocal\n";
+    print `sbt  -DsparkVersion=$spark_version version clean test +publishLocal`;
+    print "\nGoing to run: sbt  -DsparkVersion=$spark_version version clean test +publishSigned\n";
+    print `sbt  -DsparkVersion=$spark_version version clean test +publishSigned`;
     print "\nbuilt\n";
 }
