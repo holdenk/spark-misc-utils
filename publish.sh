095b2349b8661ff912ce3ab945ad651a4bafcfff
Holden Karau <holden@pigscanfly.ca>
2024-06-06 18:17:08 -0700

    Publish more versions

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 set -ex
-for spark_version in 2.4.0 3.0.0 3.1.1 3.2.0
+for spark_version in 3.0.0 3.1.1 3.2.0 3.3.1 3.4.1 3.5.1
 do
   sbt  -DsparkVersion=$spark_version version clean +publishLocal +publishSigned
 done
